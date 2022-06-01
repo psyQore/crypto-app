970d121a9a83a839a8be1b7284a827704778c638
Juan Galarce <j6alarce@gmail.com>
2022-06-01 11:04:17 -0400

    Se crea el container, se importa img, se crea el heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import { useState } from "react";
 import styled from "@emotion/styled";
+import CryptoImg from "./img/imagen-criptos.png";
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+`;
 
 const Heading = styled.h1`
   font-family: "Lato", sans-serif;
   color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `;
 
 function App() {
   return (
-    <div>
-      <Heading>Hello World</Heading>
-    </div>
+    <Container>
+      <Image src={CryptoImg} alt="crypto" />
+      <div>
+        <Heading>Cotiza Critomonedas al Instante</Heading>
+      </div>
+    </Container>
   );
 }
 
