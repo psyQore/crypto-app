193dfd856d6d1f37aa6360305ef5e906473c6f6f
Juan Galarce <j6alarce@gmail.com>
2022-06-02 10:42:18 -0400

    Se agregan estilos al hook select

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,6 +11,7 @@ const ImputSubmit = styled.input`
   font-weight: 700;
   text-transform: uppercase;
   font-size: 20px;
+  margin-top: 30px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
 
--- src/hooks/useSelectCoins.jsx
+++ src/hooks/useSelectCoins.jsx
@@ -2,20 +2,32 @@ import styled from "@emotion/styled";
 
 const Label = styled.label`
   color: #fff;
+  display: block;
+  font-family: "Lato", sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  margin: 15px 0px;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  font-size: 18px;
+  padding: 14px;
+  border-radius: 10px;
 `;
 
 const useSelectCoins = (label, options) => {
   const SelectCoins = () => (
     <>
       <Label>{label}</Label>
-      <select>
+      <Select>
         <option value="">Seleccione</option>
         {options.map((option) => (
           <option key={option.id} value={option.id}>
             {option.name}
           </option>
         ))}
-      </select>
+      </Select>
     </>
   );
 
